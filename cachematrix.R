f8472b7359e51b50b4eb261fafc2bea721fae626
shsab <sh.sabzevari@gmail.com>
2015-04-26 15:01:18 +0800

    Implementing Cacheable matrix inverse function
    The function is restricted for sake of assignment. If EXP and tol are
    converted to parameters then the function is capable of calculating any
    exponent of the matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cretae a special matrix that contains the matrix and 
+## Caches its invert.
 
-## Write a short comment describing this function
-
+## Assume matrix provided is invertable
 makeCacheMatrix <- function(x = matrix()) {
-
+  xInv <- NULL
+  set <- function(Y){
+    x <<- y
+    xInv <<- NULL
+  }
+  get <- function() x
+  setInvert <- function(xInvert) xInv <- xInvert
+  getInvert <- function() xInv
+  list(set = set, get = get,
+       setInvert = setInvert,
+       getInvert = getInvert)
 }
 
 
-## Write a short comment describing this function
+## Check if invert exists in chache return it, otherwise calculate
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  xInv <- x$getInvert()
+  if(!is.null(xInv)) {
+    message("getting cached data")
+    return(xInv)
+  }
+  MAT <- x$get()
+  tol <- Null
+  EXP <- -1
+  #MAT <- as.matrix(MAT)
+  matdim <- dim(MAT)
+  if(is.null(tol)){
+    tol=min(1e-7, .Machine$double.eps*max(matdim)*max(MAT))
+  }
+  if(matdim[1]>=matdim[2]){ 
+    svd1 <- svd(MAT)
+    keep <- which(svd1$d > tol)
+    xInv <- t(svd1$u[,keep]%*%diag(svd1$d[keep]^EXP, nrow=length(keep))%*%t(svd1$v[,keep]))
+  }
+  if(matdim[1]<matdim[2]){ 
+    svd1 <- svd(t(MAT))
+    keep <- which(svd1$d > tol)
+    xInv <- svd1$u[,keep]%*%diag(svd1$d[keep]^EXP, nrow=length(keep))%*%t(svd1$v[,keep])
+  }
+  x$setInvert(xInv)
+  xInv
 }
